49b099af244329dc0967e96cff55f0e2ffa6b353
sinsangwoo <aksrkd7191@gmail.com>
2026-02-18 02:28:25 +0900

    chore: export Phase 1 classes from top-level package, bump version to 0.4.0

--- src/gradient_pathology/__init__.py
+++ src/gradient_pathology/__init__.py
@@ -2,6 +2,17 @@
 
 from gradient_pathology.analyzer import GradientAnalyzer
 from gradient_pathology.core import GradientReport
+from gradient_pathology.finetuning import (
+    AdapterMonitor,
+    ForgettingDetector,
+    LoRARankTracker,
+)
 
-__version__ = "0.1.0"
-__all__ = ["GradientAnalyzer", "GradientReport"]
+__version__ = "0.4.0"
+__all__ = [
+    "GradientAnalyzer",
+    "GradientReport",
+    "LoRARankTracker",
+    "AdapterMonitor",
+    "ForgettingDetector",
+]
